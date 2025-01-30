3fa5ef2cdc991d9f6eb922ea9256d32964cd6bd7
SufiyanAbbasi <sufiyantahirabbasi1122@gmail.com>
2025-01-30 17:07:33 +0500

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ChartComponent } from "./chart/chart.component";
+import { ChartComponent } from "./components/chart/chart.component";
 import { CommonModule } from '@angular/common';
 
 @Component({
